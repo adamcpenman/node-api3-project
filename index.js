70d83714806db9ff415d081c1efd1252c3e3f2c1
Adam <adamcpenman@gmail.com>
2019-12-10 08:00:39 -0600

    initial commit, added started code to index

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code away!
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+server.get("/", (req, res) => {
+    // console.log("ip:", req.ip)
+    res.send("<h2>Project Node 3</h3>")
+})
+
+server.listen(4000, () => {
+  console.log("Server Running on http://localhost:4000")
+})
